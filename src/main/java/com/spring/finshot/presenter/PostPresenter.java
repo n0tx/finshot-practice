7f49138944abe87796e45e2c6dd4aa1f43ec8979
n0tx <rcandra91@msn.com>
2025-02-17 19:52:12 +0700

    - replace with single class import

--- src/main/java/com/spring/finshot/presenter/PostPresenter.java
+++ src/main/java/com/spring/finshot/presenter/PostPresenter.java
@@ -3,7 +3,14 @@ package com.spring.finshot.presenter;
 import com.spring.finshot.entity.Post;
 import com.spring.finshot.service.PostServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
