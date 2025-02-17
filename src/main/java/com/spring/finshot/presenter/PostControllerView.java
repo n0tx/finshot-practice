ee22c0e9935633c977d53bf3dbb02d58c494a1f0
n0tx <rcandra91@msn.com>
2025-02-18 01:20:14 +0700

    replace with single class imports

--- src/main/java/com/spring/finshot/presenter/PostControllerView.java
+++ src/main/java/com/spring/finshot/presenter/PostControllerView.java
@@ -4,7 +4,11 @@ import com.spring.finshot.entity.Post;
 import com.spring.finshot.utils.ApiResponseHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
